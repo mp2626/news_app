cadd73b515301f68b307c90d1bbce453352c8edc
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-04-08 21:53:57 +1000

    update to renderTopStories feature, added a count to allow user to get the next 5 articals in the list

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // Top Stories Vars
 const newCard = $("#newCards");
+const search = $("#search")
+
+let topNewsMin = 0;
+let topNewsMax = 5;
 
 // Top Stories API Var
 const topStoriesAPI = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=Va9UoQ7BSpY4GzfHt7uLq6ZX16HCjwu2";
@@ -15,41 +19,51 @@ function getTopStories(search) {
             };
         })
         .then(data => {
-            renderTopStories(data);
+            topNews = data;
+            renderTopStories(topNews);
         });
 }
 // Top Stories Card Render Function
 
-function renderTopStories(data) {
-
+function renderTopStories(topNews) {
     // clear section
 
     // vars
-    news = data.results;
-    console.log(data.results);
+    news = topNews.results;
+    console.log(topNews.results);
 
     // loop to gather data and create feed
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < news.length; i++) {
 
-        tile = news[i].title
-        console.log(tile);
+        if (i >= topNewsMin && i < topNewsMax) {
 
-        newsAbstract = news[i].abstract;
-        console.log(newsAbstract);
+            tile = news[i].title;
+            console.log(tile);
 
-        author = news[i].byline;
-        console.log(Author);
+            newsAbstract = news[i].abstract;
+            console.log(newsAbstract);
 
-        articleUrl = news[i].url;
-        console.log(articleUrl);
+            author = news[i].byline;
+            console.log(author);
 
-        newsLocation = news[i].section;
-        console.log(newsLocation);
+            articleUrl = news[i].url;
+            console.log(articleUrl);
 
-        newsLocalLocation = news[i].subsection
-        console.log(newsLocalLocation);
+            newsLocation = news[i].section;
+            console.log(newsLocation);
 
+            newsLocalLocation = news[i].subsection;
+            console.log(newsLocalLocation);
+        }
     }
+    topNewsMin += 5;
+    topNewsMax += 5;
+    console.log(topNewsMin, topNewsMax);
 }
 
 getTopStories();
+
+search.on("click", (event) => {
+    event.preventDefault();
+    renderTopStories(topNews);
+});
