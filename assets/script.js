b0b9a594bf9cc246c0d641a2f01db39461886e17
‘zhou <anthony0716@hotmail.com>
2021-04-12 12:03:31 +1000

    first PR from ZT

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,99 @@
-//API
+// Top Stories Vars
+const newCard = $("#newCards");
+const nextNews = $("#topNews");
+const toDaysDate = $("#date");
+
+// date 
+toDaysDate.text(moment().format('ddd Do MMM, YYYY'));
+// bootstrap
+
+let topNewsMin = 0;
+let topNewsMax = 5;
+
+// Top Stories API Var
+const topStoriesAPI = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=Va9UoQ7BSpY4GzfHt7uLq6ZX16HCjwu2";
+
+// Top Stories Fetch API Function
+function getTopStories(search) {
+    fetch(topStoriesAPI)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert("No result found")
+            };
+        })
+        .then(data => {
+            topNews = data;
+            renderTopStories(topNews);
+        });
+}
+// Top Stories Card Render Function
+
+function renderTopStories(topNews) {
+    // clear section
+    newCard.children().remove('div');
+    // vars
+    news = topNews.results;
+    //console.log(topNews.results);
+
+    // loop to gather data and create feed
+    for (i = 0; i < news.length; i++) {
+
+        if (i >= topNewsMin && i < topNewsMax) {
+
+            tile = news[i].title;
+            newsAbstract = news[i].abstract;
+            author = news[i].byline;
+            articleUrl = news[i].url;
+            newsLocalLocation = news[i].subsection;
+            if (newsLocalLocation != "") {
+                capNewsLocalLocation = newsLocalLocation[0].toUpperCase() + newsLocalLocation.slice(1);
+            }
+            articleUrl = news[i].url;
+
+            // create elements
+            newCardDiv = $('<div>').addClass('card col-11 col-md-11 col-lg-4');
+            cardTile = $('<h1>').addClass('card-header').text(tile);
+            cardLocation = $('<h3>').text(capNewsLocalLocation);
+            cardBody = $('<div>').addClass('card-body');
+            cardAbstract = $('<p>').text(newsAbstract);
+            cardAuthor = $('<h2>').text(author);
+            cardsUrlButton = $('<a>').text('Article').attr('href', articleUrl).attr('target', '""').addClass('btn btn-light')
+            // Build cards
+            cardBody.append(cardBody, cardLocation, cardAbstract, cardAuthor, cardsUrlButton);
+            newCardDiv.append(cardTile, cardBody);
+            newCard.append(newCardDiv);
+        }
+    }
+
+    if (topNewsMin < news.length) {
+        topNewsMin += 5;
+        topNewsMax += 5;
+        //console.log(topNewsMin, topNewsMax);
+    }
+}
+
+function scroll() {
+    window.scrollTo(0, 200);
+}
+
+nextNews.on("click", (event) => {
+    event.preventDefault();
+    renderTopStories(topNews);
+    scroll();
+});
+
+getTopStories();
+
+
+
+
+
+//ZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZT
+//Article Search API Script below
+//ZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZTZT
+
 const zhouTianKey = "gfXdGsZ9MrEsXZPtKlAv5IB6NM2ImZQ6";
 //DOM elements
 var searchModalEl = $('#searchModal');
@@ -29,7 +124,7 @@ $('#modalBtn').on('click', modalUpdate);
 //---------------------------------------------------------------------------------------------------------------------
 //Load previous search and create drop down buttons
 function modalUpdate(){
-  $('#dropdownBtn').children().remove();
+  $('#dropdownBtn').children().remove(); //Needs to clean up buttons generated from previous event click.
   if (storedSearch) {historyBtns()} else {return};
 }
 //---------------------------------------------------------------------------------------------------------------------
@@ -95,6 +190,7 @@ function displayArticles(artKey, artSort, newsDesk, artBegin, artEnd) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data.response.docs);
+          if(!data.response.docs.length){alert("Result Not Found, please make new searches.");return}
           for(var i=0; i<data.response.docs.length; i++){
             var artEl = $('<div>').addClass('article');
             artCardsEl.append(artEl);
@@ -104,7 +200,8 @@ function displayArticles(artKey, artSort, newsDesk, artBegin, artEnd) {
             var artTitleEl = $('<h2>').addClass('artTitle').text(data.response.docs[i].headline.main);
             var snippetEl = $('<h3>').text(data.response.docs[i].snippet);
             var authorEl = $('<h4>').text(data.response.docs[i].byline.original);
-            var artLinkEl = $('<a>').addClass('artLink').attr("href",data.response.docs[i].web_url).text("Article");
+            var artLinkEl = $('<a>').addClass('artLink').attr("href", data.response.docs[i].web_url).text("Article");
+            artLinkEl.attr("target", "_blank")
             artEl.append(artTypeEl,dateEl,artTitleEl,snippetEl,authorEl,artLinkEl);
           };
         });
@@ -135,7 +232,9 @@ $( function() {
       .datepicker({
         defaultDate: "+1w",
         changeMonth: true,
+        changeYear: true,
         numberOfMonths: 1,
+        maxDate: 0,
         dateFormat: "yymmdd"
       })
       .on( "change", function() {
@@ -144,7 +243,9 @@ $( function() {
     to = $("#end-date-input").datepicker({
       defaultDate: "+1w",
       changeMonth: true,
+      changeYear: true,
       numberOfMonths: 1,
+      maxDate: 0,
       dateFormat: "yymmdd"
     })
     .on( "change", function() {
